33255055bc8add578f3c7d48921b646bb3883109
Benedict.ESN <elder@elderscrolls.net>
2024-06-29 10:41:30 +0300

    first commit

---
+++ src/main/java/ru/netology/Person.java
@@ -0,0 +1,75 @@
+package ru.netology;
+
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    private final String name;
+    private final String surname;
+    private Integer age;
+    private String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this(name, surname);
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст не может быть отрицательным");
+        }
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age != null;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return age != null ? OptionalInt.of(age) : OptionalInt.empty();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (age != null) {
+            age++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(this.surname)
+                .setAddress(this.address)
+                .setAge(0);
+    }
+}
---
+++ src/main/java/ru/netology/PersonBuilder.java
@@ -0,0 +1,45 @@
+package ru.netology;
+
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private Integer age;
+    private String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст не может быть отрицательным");
+        }
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("имя и фамилия должны быть обязательно определены");
+        }
+        Person person;
+        if (age != null) {
+            person = new Person(name, surname, age);
+        } else {
+            person = new Person(name, surname);
+        }
+        person.setAddress(address);
+        return person;
+    }
+}
